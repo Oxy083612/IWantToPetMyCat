7174f32c5a3025212f3f156f6892fea1e9b1425e
MagrosThornrime <magros.thornrime@gmail.com>
2025-04-08 06:44:17 +0200

    Remove unused print

--- scripts/item_gathering/pickup_item.gd
+++ scripts/item_gathering/pickup_item.gd
@@ -10,7 +10,6 @@ func _ready():
 	
 func _on_item_generation_change_item(item_name) -> void:
 	var item = ItemsPool.items[item_name]
-	print(item_name, " ", item["texture"])
 	sprite.texture = load(item["texture"])
 	parent.item_name = item_name
 	item_generation.change_item.disconnect(_on_item_generation_change_item)
