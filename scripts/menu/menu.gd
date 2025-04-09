b122e7f53b0f265227031427cdc81c5af6c7653d
MagrosThornrime <magros.thornrime@gmail.com>
2025-04-09 02:42:45 +0200

    Fix all remaining collisions

--- scripts/menu/menu.gd
+++ scripts/menu/menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _ready():
 	Music.stream.loop = true
-	Music.play()
+	#Music.play() # TODO: uncomment to make music work
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/item_gathering/gathering_scene.tscn")
