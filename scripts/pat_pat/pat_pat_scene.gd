20332c2bfe6a94477fe79dd475a1389daeb7fab1
RoseThornrime <magros.thornrime@gmail.com>
2026-03-10 02:20:15 +0100

    THE CAT CAN BE PETTED

--- scripts/pat_pat/pat_pat_scene.gd
+++ scripts/pat_pat/pat_pat_scene.gd
@@ -5,16 +5,25 @@ extends Node2D
 @onready var last_mouse_position = Vector2(0, 0)
 @onready var purr_stream = $AudioStreamPlayer2D
 @onready var purring = false
-@onready var cat = $Sprite2D
-@onready var hand_angle = 315
+@onready var cat_position = $Sprite2D.global_position - Vector2(0, 50)
+@onready var cat_radius = $Sprite2D/Area2D/CollisionShape2D.shape.radius * 5
+@onready var hand_angle = 330.0
+@onready var hand_default = Hand.global_position
+@onready var angle_speed = 2.0
+@onready var current_angle = 0.0
+
 
 func _ready() -> void:
-	var cat_position = cat.global_position
 	var hand_length = Hand.targetPosition - Hand.global_position
-	var length_vector = Vector2(hand_length.x, 0)
+	var length_vector = Vector2(hand_length.x, 0.0)
 	length_vector = length_vector.rotated(deg_to_rad(hand_angle))
 	Hand.global_rotation = deg_to_rad(hand_angle)
+	var radius_vector = Vector2(0, cat_radius)
+	radius_vector = radius_vector
 	Hand.global_position = cat_position - length_vector
+	hand_default = Hand.global_position
+	Hand.global_position += Vector2(cat_radius, 0.0)
+	
 
 func _input(event) -> void:
 	if event is not InputEventMouseButton:
@@ -37,12 +46,12 @@ func can_be_petted(current_mouse_position: Vector2) -> bool:
 func _physics_process(delta: float) -> void:
 	var current_mouse_position = get_viewport().get_mouse_position()
 	if can_be_petted(current_mouse_position):
+		current_angle += delta * angle_speed
+		var x_increment = cat_radius * cos(current_angle)
+		var y_increment = cat_radius * sin(current_angle)
+		var increment = Vector2(x_increment, y_increment)
+		Hand.global_position = hand_default + increment
 		if not purring:
-			print("meow")
-			print("mouse", current_mouse_position)
-			print("target", Hand.targetPosition)
-			print("last", Hand.lastPosition)
-			print("global", Hand.global_position)
 			purring = true
 			purr_stream.play()
 	else:
