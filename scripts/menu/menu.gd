bc1c1cd741a07460839a6e9438d9f98fdddb2cf1
RoseThornrime <magros.thornrime@gmail.com>
2026-03-11 18:00:17 +0100

    Add audio on/off button

--- scripts/menu/menu.gd
+++ scripts/menu/menu.gd
@@ -1,5 +1,11 @@
 extends Control
 
+
+@onready var audio_label = $Audio/Label
+@onready var is_audio_muted = false
+@onready var master_bus_idx = AudioServer.get_bus_index("Master")
+
+
 func _ready():
 	Music.stream.loop = true
 	if not Music.playing:
@@ -10,3 +16,17 @@ func _on_play_pressed() -> void:
 
 func _on_credits_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu/credits.tscn")
+
+
+func _on_audio_pressed() -> void:
+	if is_audio_muted:
+		is_audio_muted = false
+		audio_label.text = "AUDIO ON"
+		AudioServer.set_bus_mute(master_bus_idx, false)
+		Music.play()
+		return
+	is_audio_muted = true
+	audio_label.text = "AUDIO OFF"
+	AudioServer.set_bus_mute(master_bus_idx, true)
+	Music.stop()
+		
